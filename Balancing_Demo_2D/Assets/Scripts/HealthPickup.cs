87c560fde8f55c9201b6170a08f2674239c16a1e
Kennedy Sovine <s.sovine@Me.com>
2025-05-08 17:48:42 +0100

    Health Pickups

---
+++ Balancing_Demo_2D/Assets/Scripts/HealthPickup.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class HealthPickup : MonoBehaviour
+{
+    [SerializeField] private float respawnTime = 30f; // 30 seconds respawn time
+    [SerializeField] private float healPercentage = 0.15f; // 15% of max health
+
+    private float disableTime = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!gameObject.activeSelf && Time.time - disableTime > respawnTime)
+        {
+            gameObject.SetActive(true); // Activate the pickup object after respawn time
+        }
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("HealthPickup: OnTriggerEnter2D called with " + other.name);
+        if (!other.CompareTag("Player")) return;
+
+        var champion = other.GetComponent<BaseChampion>();
+        if (champion != null)
+        {
+            // Heal the champion by defined percentage of their max health
+            champion.UpdateHealthRpc(champion.maxHealth.Value * healPercentage);
+            
+            // Record the time when we disable the pickup
+            disableTime = Time.time;
+            gameObject.SetActive(false); // Deactivate the pickup object
+        }
+        else
+        {
+            Debug.LogWarning("No BaseChampion component found on the player.");
+        }
+    }
+}
