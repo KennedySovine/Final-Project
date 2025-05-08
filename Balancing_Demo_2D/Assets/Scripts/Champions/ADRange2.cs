0d001541112e6e65e21829366ce0e1909401bb75
Kennedy Sovine <s.sovine@Me.com>
2025-05-08 16:06:30 +0100

    ADRange2 Optimize

--- Balancing_Demo_2D/Assets/Scripts/Champions/ADRange2.cs
+++ Balancing_Demo_2D/Assets/Scripts/Champions/ADRange2.cs
@@ -4,10 +4,12 @@ using System.Collections;
 
 public class ADRange2 : BaseChampion
 {
-
+    #region Fields and Properties
     [Header("Champion Settings")]
     public int attackStacks = 0; // Gwen generates passive stacks per auto attack.
+    #endregion
 
+    #region Initialization Methods
     void Start()
     {
         base.Start();
@@ -18,7 +20,6 @@ public class ADRange2 : BaseChampion
             attackSpeed.OnValueChanged += (previousValue, newValue) => { // Update the attack speed value
                 updateAbility1CooldownRpc(newValue); // Update the ability cooldown based on attack speed
             };
-
         }
     }
 
@@ -46,55 +47,8 @@ public class ADRange2 : BaseChampion
 
         //Other stuff
         stackDuration.Value = 4f;
-
         rapidFire.Value = 1;
         maxStacks.Value = 4; // Maximum number of stacks for the ability
-
-    }
-
-    public override void Update(){
-        base.Update(); // Call the base class Update method
-
-        updateIsEmpoweredRpc(true);
-        // Ashe is always 'empowered' so she can always apply frost.
-
-        //Ashe's ranger's focus cooldown is dependent on attackspeed
-    }
-
-    public override void stackManager(){
-        // 1 stack expires after 1 second
-        if (stackCount.Value > 0){
-            if (Time.time > stackStartTime.Value + stackDuration.Value) // If the stack timer is up
-            {
-                updateStackCountRpc(-1, stackCount.Value, maxStacks.Value);
-            }
-        }
-    }
-    //Slow Logic
-    public override GameObject empowerLogic(GameObject bullet)
-    {
-        var bulletComponent = bullet.GetComponent<Bullet>();
-        if (bulletComponent != null){
-            bulletComponent.slowAmount = 0.2f; // Set the slow amount to 20%
-        }
-        return bullet;
-    }
-
-    public override GameObject critLogic(GameObject bullet){
-        // CRIT DOES FROST AND DOES NOT DO DMG
-        float chance = Random.Range(0f, 1f);
-        var bulletComponent = bullet.GetComponent<Bullet>();
-        if (chance <= critChance.Value) // If the random chance is less than or equal to critChance
-        {
-            Debug.Log("Critical hit! Critical Frost applied.");
-            bulletComponent.slowAmount = 0.4f; // Set the slow amount to 40%
-        }
-        else
-        {
-            Debug.Log("Normal hit. No critical damage.");
-        }
-
-        return bullet;
     }
 
     private void AddAbilities()
@@ -110,47 +64,61 @@ public class ADRange2 : BaseChampion
         ability1 = new Ability(
             "Rapid Frost",
             "ACTIVE: For 6 seconds, gain <i>25% bonus attack speed<i> and fire 5 shots rapidly. Applies <i>21<i> AD per arrow. Cannot cast unless there are 4 stacks of Focus",
-            0f, // Cooldown in seconds
-            30f, // Mana cost
-            0f  // No range
+            0f, 
+            30f, 
+            0f 
         );
-
-        ability1.icon = Resources.Load<Sprite>("Sprites/Ashe_RangerFocus_Default"); // Load the icon for the ability from Resources folder
-        ability1.icon2 = Resources.Load<Sprite>("Sprites/Ashe_RangerFocus_Empowered"); // Load the icon for the ability from Resources folder
+        ability1.icon = Resources.Load<Sprite>("Sprites/Ashe_RangerFocus_Default");
+        ability1.icon2 = Resources.Load<Sprite>("Sprites/Ashe_RangerFocus_Empowered");
         
         ability2 = new Ability(
             "Ranger's Focus",
             "PASSIVE: Basic attacks generate a stack of Focus for 4 seconds, which refreshes on additional attacks and stacks up to 4, expriring after a second.",
-            0f, // Cooldown in seconds
-            0f, // Mana cost
-            0f // No range
-            
+            0f, 
+            0f,
+            0f 
         );
-
-        ability2.icon = Resources.Load<Sprite>("Sprites/Ashe_Enchanted_Crystal_Arrow"); // Load the icon for the ability from Resources folder
+        ability2.icon = Resources.Load<Sprite>("Sprites/Ashe_Enchanted_Crystal_Arrow");
 
         ability3 = new Ability(
             "Volley",
             "Next arrow applies critical Frost and deals extra physical damage.",
-            18f, // Cooldown in seconds
-            75f, // Mana cost
-            0f // Range
+            18f,
+            75f, 
+            0f
         );
+        ability3.icon = Resources.Load<Sprite>("Sprites/Ashe_Volley");
+        ability3.setDuration(6f);
 
-        autoAttack.setRange(20f); // Set the range of the auto attack ability
-
-        ability3.icon = Resources.Load<Sprite>("Sprites/Ashe_Volley"); // Load the icon for the ability from Resources folder
-
+        autoAttack.setRange(20f); 
         ability1.setDuration(6f);
+        passive.Stats.championType = championType;
 
-        passive.Stats.championType = championType; // Set the champion type for the passive ability
-
-        abilityDict.Add("Q", ability1); // Add the ability to the UI manager
-        abilityDict.Add("W", ability2); // Add the ability to the UI manager
-        abilityDict.Add("E", ability3); // Add the ability to the UI manager
+        abilityDict.Add("Q", ability1);
+        abilityDict.Add("W", ability2); 
+        abilityDict.Add("E", ability3);
 
         SendToUI();
+    }
+    #endregion
 
+    #region Core Game Loop Methods
+    public override void Update(){
+        base.Update();
+
+        updateIsEmpoweredRpc(true);
+        // Ashe is always 'empowered' so she can always apply frost.
+        // Ashe's ranger's focus cooldown is dependent on attackspeed
+    }
+
+    public override void stackManager(){
+        // 1 stack expires after 1 second
+        if (stackCount.Value > 0){
+            if (Time.time > stackStartTime.Value + stackDuration.Value) // If the stack timer is up
+            {
+                updateStackCountRpc(-1, stackCount.Value, maxStacks.Value);
+            }
+        }
     }
 
     public override void abilityIconCooldownManaChecks()
@@ -190,25 +158,71 @@ public class ADRange2 : BaseChampion
             }
         }
     }
+    #endregion
 
-    public virtual GameObject ability3Logic(GameObject bullet){
+    #region Ability Logic Methods
+    //Slow Logic
+    public override GameObject empowerLogic(GameObject bullet)
+    {
+        var bulletComponent = bullet.GetComponent<Bullet>();
+        if (bulletComponent != null){
+            bulletComponent.slowAmount = 0.2f; // Set the slow amount to 20%
+        }
+        return bullet;
+    }
+
+    public override GameObject critLogic(GameObject bullet){
+        // CRIT DOES FROST AND DOES NOT DO DMG
+        float chance = Random.Range(0f, 1f);
+        var bulletComponent = bullet.GetComponent<Bullet>();
+        if (chance <= critChance.Value) // If the random chance is less than or equal to critChance
+        {
+            Debug.Log("Critical hit! Critical Frost applied.");
+            bulletComponent.slowAmount = 0.4f; // Set the slow amount to 40%
+        }
+        else
+        {
+            Debug.Log("Normal hit. No critical damage.");
+        }
+
+        return bullet;
+    }
+
+    public override GameObject ability3Logic(GameObject bullet){
         var bulletComponent = bullet.GetComponent<Bullet>();
         if (bulletComponent != null){
             bulletComponent.ADDamage = 20f + AD.Value; 
             bulletComponent.slowAmount = 0.4f; // Set the slow amount to 40%
-        
         }
 
         return bullet; // Return the modified bullet
     }
 
+    private IEnumerator RapidFireCoroutine(float duration)
+    {
+        var tempAS = attackSpeed.Value; // Store the original attack speed
+        var tempAD = AD.Value; // Store the original AD
+        Debug.Log("Rapid Fire started!");
+        updateAttackSpeedRpc(0.25f); // Increase attack speed by 25
+        updateADRpc(.21f); // Increase AD by 21
+        yield return new WaitForSeconds(duration); // Wait for the specified duration
+        Debug.Log("Rapid Fire ended!");
+
+        // Reset rapid fire state
+        updateRapidFireRpc(1);
+        attackSpeed.Value = tempAS; // Reset attack speed to original value
+        AD.Value = tempAD; // Reset AD to original value
+        Debug.Log("Rapid Fire state reset.");
+    }
+    #endregion
+
+    #region RPC Methods
     [Rpc(SendTo.Server)]
     public override void passiveAbilityRpc(){
         // Add a slow thing in base champion.
         // Frost = 20% slow for 2 seconds
         // Additional frost damage = 155% of crit chance as AD damage
-        //Look at empowerLogic
-        
+        // Look at empowerLogic
     }
 
     [Rpc(SendTo.Server)]
@@ -259,23 +273,6 @@ public class ADRange2 : BaseChampion
         ability3.Stats.totalManaSpent += ability3.manaCost; // Update the total mana spent for the ability
     }
 
-    private IEnumerator RapidFireCoroutine(float duration)
-    {
-        var tempAS = attackSpeed.Value; // Store the original attack speed
-        var tempAD = AD.Value; // Store the original AD
-        Debug.Log("Rapid Fire started!");
-        updateAttackSpeedRpc(0.25f); // Increase attack speed by 25
-        updateADRpc(.21f); // Increase AD by 21
-        yield return new WaitForSeconds(duration); // Wait for the specified duration
-        Debug.Log("Rapid Fire ended!");
-
-        // Reset rapid fire state
-        updateRapidFireRpc(1);
-        attackSpeed.Value = tempAS; // Reset attack speed to original value
-        AD.Value = tempAD; // Reset AD to original value
-        Debug.Log("Rapid Fire state reset.");
-    }
-
     [Rpc(SendTo.Server)]
     public void updateAbility1CooldownRpc(float attackSpeed)
     {
@@ -306,4 +303,5 @@ public class ADRange2 : BaseChampion
             ability1.setCooldown(1.6f);
         }
     }
+    #endregion
 }
